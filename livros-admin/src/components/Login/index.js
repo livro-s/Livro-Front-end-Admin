32535f3672a298155b7000258b59acb1fe11643e
Srim <deamoon0801@naver.com>
2020-11-23 23:49:31 +0900

    [ADD] <components/Login/index.js> component

--- livros-admin/src/components/Login/index.js
+++ livros-admin/src/components/Login/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Login from "./Login";
 
 const index = () => {
     return (
         <div>
-            
+            <Login></Login>
         </div>
     );
 };
