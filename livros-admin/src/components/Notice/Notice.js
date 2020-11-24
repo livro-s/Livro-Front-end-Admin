f463f126e0515d8e1bc837343f2820e353c6c8c2
Srim <deamoon0801@naver.com>
2020-11-24 15:15:48 +0900

    [ADD] <components/Notice/Notice.js> getNotice feature

--- livros-admin/src/components/Notice/Notice.js
+++ livros-admin/src/components/Notice/Notice.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import NoticeItem from "./NoticeItem";
 import * as G from "../../assets/style/GlobalStyle";
 import * as S from "../../assets/style/Notice/Notice";
+import { getNotice } from "../../api/notice";
 
 const Notice = () => {
-    return (
-        <G.AllContainer>
-            <S.NoticeContainer>
-                <NoticeItem></NoticeItem>
-                <NoticeItem></NoticeItem>
-                <NoticeItem></NoticeItem>
-            </S.NoticeContainer>
+  const [notice, setNotice] = useState([]);
+  const [page, setPage] = useState(1);
 
-            <S.NoticeContainer page>
-                <S.PageBox>{"<"}</S.PageBox>
-                <S.PageBox num>1</S.PageBox>
-                <S.PageBox>{">"}</S.PageBox>
-            </S.NoticeContainer>
-        </G.AllContainer>
-    );
+  useEffect(() => {
+    getNotice(page)
+      .then((res) => {
+        console.log(res);
+        setNotice(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [page]);
+
+  const onDecreasePage = () => {
+    if (page > 1) setPage((state) => state - 1);
+  };
+
+  const onIncreasePage = () => {
+    setPage((state) => state + 1);
+  };
+
+  return (
+    <G.AllContainer>
+      <S.NoticeContainer>
+        {notice.map((n) => (
+          <NoticeItem
+            id={n.uuid}
+            title={n.title}
+            content={n.content}
+            date={n.createdAt}
+          ></NoticeItem>
+        ))}
+      </S.NoticeContainer>
+
+      <S.NoticeContainer page>
+        <S.PageBox onClick={onDecreasePage}>{"<"}</S.PageBox>
+        <S.PageBox num>{page}</S.PageBox>
+        <S.PageBox onClick={onIncreasePage}>{">"}</S.PageBox>
+      </S.NoticeContainer>
+    </G.AllContainer>
+  );
 };
 
 export default Notice;
