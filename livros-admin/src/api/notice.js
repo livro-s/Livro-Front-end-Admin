43c7cb974cce3416cba5e95f4814c0484b6eb39f
Srim <deamoon0801@naver.com>
2020-11-24 20:51:29 +0900

    [ADD] <api/notice.js> detailNotice api

--- livros-admin/src/api/notice.js
+++ livros-admin/src/api/notice.js
@@ -1,17 +1,21 @@
 import { getClientAccessToken } from "./client";
 
 export const addNotice = (title, content) => {
-    return getClientAccessToken.post("/admin/notice", {title, content})
+  return getClientAccessToken.post("/admin/notice", { title, content });
 };
 
 export const deleteNotice = (id) => {
-    return getClientAccessToken.delete("/admin/notice/" + id)
+  return getClientAccessToken.delete("/admin/notice/" + id);
 };
 
 export const putNotice = (id, title, content) => {
-    return getClientAccessToken.put("/admin/notice/" + id, {title, content})
+  return getClientAccessToken.put("/admin/notice/" + id, { title, content });
 };
 
 export const getNotice = (page) => {
-    return getClientAccessToken.get("/notice?page=" + page)
-}
+  return getClientAccessToken.get("/notice?page=" + page);
+};
+
+export const detailGetNotice = (id) => {
+  return getClientAccessToken.get("/notice/" + id);
+};
