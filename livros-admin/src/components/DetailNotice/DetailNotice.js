9d63bf07548925a100caa6ea73480bb7afa21f56
Srim <deamoon0801@naver.com>
2020-11-24 10:30:43 +0900

    [ADD] <components/DetailNotice/DetailNotice.js> component

---
+++ livros-admin/src/components/DetailNotice/DetailNotice.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as G from "../../assets/style/GlobalStyle";
+import * as S from "../../assets/style/Notice/DetailNotice";
+
+const DetailNotice = () => {
+    return (
+        <G.AllContainer>
+            <S.NoticeTitle>도서관 이용 공지사항 안내</S.NoticeTitle>
+            <S.NoticeText>2020년 11월 24일</S.NoticeText>
+            <S.NoticeText contents>얼음이 되려니와, 과실이 넣는 같은 이상은 이상, 인류의 하였으며, 뿐이다. 
+                그들은 우리의 그들은 구하지 목숨을 이는 위하여서, 쓸쓸하랴? 낙원을 피가 간
+                에 있는 얼음에 수 보이는 위하여서. 얼마나 없으면 주며, 튼튼하며, 가슴이 
+                곳이 위하여 밝은 것은 것이다. 영락과 대한 가지에 불어 힘있다. 구하지 그들은
+                 사라지지 같이, 관현악이며, 구하기 때문이다. 가장 청춘의 물방아 피고, 가는
+                  방황하여도, 그것을 이 때에, 아니다. 
+                피어나는 새가 것은 그들은 못할 간에 미묘한 봄바람을 튼튼하며, 철환하였는가?
+                 심장은 청춘에서만 청춘의 피가 가는 작고 그들은 수 살 것이다. 인간에 설산
+                 에서 가장 대한 청춘의 끓는 같으며, 철환하였는가? 두손을 찬미를 가장 구하
+                 지 그들의 청춘 풍부하게 고동을 황금시대다.</S.NoticeText>
+        </G.AllContainer>
+    );
+};
+
+export default DetailNotice;
