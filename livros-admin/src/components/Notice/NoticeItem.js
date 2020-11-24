b03ad7c7f50114399f650798999df8fd27e129e1
Srim <deamoon0801@naver.com>
2020-11-24 16:41:26 +0900

    [ADD] <components/Notice/NoticeItem.js> noticeView feature

--- livros-admin/src/components/Notice/NoticeItem.js
+++ livros-admin/src/components/Notice/NoticeItem.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React from "react";
 import * as S from "../../assets/style/Notice/NoticeItem";
+import { deleteNotice } from "../../api/notice";
+import { Link } from "react-router-dom";
 
-const NoticeItem = () => {
-    return (
-        <S.NoticeItemBox>
-            <S.NoticeTitle>도서관 이용 공지사항 안내</S.NoticeTitle>
-            <S.NoticeContents>이성은 있는 있을 행복스럽고 실현에 것이다. 소리다.이것은 인도하겠다는 
-                능히 날카로우나 인생에 천고에 든 그들은 아니다. 꽃이 창공에 대고, 
-                풀이 것이다. 안고, 속에서 뜨고 어쩌구 이성은 있는 있을 행복스럽고 
-                실현에 것이다. 소리다.이것은 인도하겠다는 능히 날카로우나 인생에 천고에 
-                든 그들은 아니다. 꽃이 창공에 대고, 풀이 것이다. 안고, 속에서 뜨고 어쩌구</S.NoticeContents>
+const NoticeItem = ({ id, title, content, date }) => {
 
+  const onDeleteNotice = () => {
+    deleteNotice(id)
+      .then(() => alert("공지 삭제가 완료되었습니다."))
+      .catch(() => alert("공지 삭제에 실패하였습니다. 다시 시도하세요."));
+  };
 
-            <S.NoticeInfoContainer>
-                <S.NoticeInfoBox>
-                    <S.NoticeInfoContents>
-                            <S.NoticeDate>2020년 8월 20일</S.NoticeDate>
-                        </S.NoticeInfoContents>
-                        
-                        <S.NoticeInfoContents>
-                            <S.NoticeTextButton>수정</S.NoticeTextButton>
-                            <S.NoticeTextButton delete>삭제</S.NoticeTextButton>
-                        </S.NoticeInfoContents>
-                </S.NoticeInfoBox>
-                    
-                
-                
-            </S.NoticeInfoContainer>
-        </S.NoticeItemBox>
-    );
+  return (
+    <S.NoticeItemBox>
+      <S.NoticeTitle>{title}</S.NoticeTitle>
+      <S.NoticeContents>{content}</S.NoticeContents>
+      <S.NoticeInfoContainer>
+        <S.NoticeInfoBox>
+          <S.NoticeInfoContents>
+            <S.NoticeDate>{date}</S.NoticeDate>
+          </S.NoticeInfoContents>
+
+          <S.NoticeInfoContents>
+            <Link
+              style={{ textDecoration: "none" }}
+              to={{
+                pathname: `/edit-notice/${id}`,
+                state: {
+                    editTitle : title,
+                    editContent : content
+                },
+              }}
+            >
+              <S.NoticeTextButton>수정</S.NoticeTextButton>
+            </Link>
+            <S.NoticeTextButton delete onClick={onDeleteNotice}>
+              삭제
+            </S.NoticeTextButton>
+          </S.NoticeInfoContents>
+        </S.NoticeInfoBox>
+      </S.NoticeInfoContainer>
+    </S.NoticeItemBox>
+  );
 };
 
 export default NoticeItem;
