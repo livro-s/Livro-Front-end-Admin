f8c7e3e7da017b88b6095423716a6cecd57fb78a
Srim <deamoon0801@naver.com>
2020-11-24 13:28:01 +0900

    [ADD] <components/Login/Login.js> login feature

--- livros-admin/src/components/Login/Login.js
+++ livros-admin/src/components/Login/Login.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from "../../assets/style/Login/Login";
 import { textLogo, loginAsset } from "../../assets/img";
+import { login } from "../../api/auth";
 
 const Login = () => {
+
+    const [inputs, setInputs] = useState({
+        userId : "",
+        password : ""
+      });
+
+    const { userId, password } = inputs;
+
+    const onChangeLogin = (e) => {
+        const { value, name } = e.target;
+        setInputs({
+        ...inputs,
+        [name] : value
+        });
+    };
+
+    const onLogin = () => {
+        return login(userId, password);
+    };
+
     return (
         <S.LoginContainer>
             <S.LoginSection>
@@ -16,16 +37,16 @@ const Login = () => {
 
                     <S.InputBox>
                         <S.LoginText>아이디</S.LoginText>
-                        <S.LoginInput></S.LoginInput>
+                        <S.LoginInput type="text" name="userId" value={userId} onChange={onChangeLogin}></S.LoginInput>
                     </S.InputBox>
 
                     <S.InputBox>
                         <S.LoginText>비밀번호</S.LoginText>
-                        <S.LoginInput></S.LoginInput>
+                        <S.LoginInput type="password" name="password" value={password} onChange={onChangeLogin}></S.LoginInput>
                     </S.InputBox>
                     
                 </S.LogoSection>
-                <S.LoginButton>로그인</S.LoginButton>
+                <S.LoginButton onClick={onLogin}>로그인</S.LoginButton>
                 </S.Div>
             </S.LoginSection>
 
