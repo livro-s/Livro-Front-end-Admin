313bb342974ea9a28271c29b45f5996505da8541
Srim <deamoon0801@naver.com>
2020-11-24 21:19:07 +0900

    [ADD] <components/Notice/Notice.js> error exception

--- livros-admin/src/components/Notice/Notice.js
+++ livros-admin/src/components/Notice/Notice.js
@@ -12,10 +12,9 @@ const Notice = () => {
   useEffect(() => {
     getNotice(page)
       .then((res) => {
-        console.log(res);
         setNotice(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch(() => alert("공지 불러오기에 실패했습니다. 다시 시도하세요."));
   }, [page]);
 
   const onDecreasePage = () => {
