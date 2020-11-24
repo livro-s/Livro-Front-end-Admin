d0651ea6cde1f9fd737b6e880b9768fbc7bf92fd
Srim <deamoon0801@naver.com>
2020-11-24 20:51:44 +0900

    [ADD] <App.js> router

--- livros-admin/src/App.js
+++ livros-admin/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Login, BookList, AddNotice, Notice } from "./components";
+import {
+  Login,
+  BookList,
+  AddNotice,
+  Notice,
+  DetailNotice,
+  Header,
+} from "./components";
 
 function App() {
   return (
     <div>
       <Router>
-        <Route path="/" component={Login} />
-        <Route path="/book-list" component={BookList}></Route>
-        <Route path="/notice" component={Notice}></Route>
-        <Route path="/add-notice" component={AddNotice}></Route>
+        <Route exact path="/" component={Login} />
+        <Header></Header>
+        <Route exact path="/book-list" component={BookList}></Route>
+        <Route exact path="/notice" component={Notice}></Route>
+        <Route exact path="/notice/:id" component={DetailNotice}></Route>
+        <Route exact path="/edit-notice/:id" component={AddNotice} />
+        <Route path="/add-notice/" component={AddNotice} />
       </Router>
     </div>
   );
