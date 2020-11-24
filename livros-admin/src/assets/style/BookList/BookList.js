819385d81ab2e22580ecbabe1a9304c2644b511e
Srim <deamoon0801@naver.com>
2020-11-24 11:59:09 +0900

    [ADD] <assets/style/BookList/BookList.js> style

---
+++ livros-admin/src/assets/style/BookList/BookList.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const BookListItemContainer = styled.div`
+    display : flex;
+    align-items : center;
+`;
+
+export const BookLine = styled.hr`
+    border : 1px solid #0F4C81;
+    margin-bottom : 0;
+`;
+
+export const BookState = styled.span`
+    color : #707070;
+    margin-right : 0.9rem;
+    font-size : 0.9rem;
+    cursor : pointer;
+`;
