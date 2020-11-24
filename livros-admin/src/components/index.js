ab4946d818635db2a63fc9cd499e33e16b107ddc
Srim <deamoon0801@naver.com>
2020-11-24 16:40:26 +0900

    [ADD] <components/index.js> export component

--- livros-admin/src/components/index.js
+++ livros-admin/src/components/index.js
@@ -3,3 +3,4 @@ export { default as BookList } from "./BookList";
 export { default as DetailNotice } from "./DetailNotice";
 export { default as AddNotice } from "./AddNotice";
 export { default as Notice } from "./Notice";
+export { default as Header } from "./Header";
