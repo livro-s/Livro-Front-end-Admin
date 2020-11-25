867366b62d8da6332a68f61bff1ffb1ef1b0206f
Srim <deamoon0801@naver.com>
2020-11-25 18:29:10 +0900

    [ADD] <assets/style/GlobalStyle.js> style

--- livros-admin/src/assets/style/GlobalStyle.js
+++ livros-admin/src/assets/style/GlobalStyle.js
@@ -25,3 +25,22 @@ export const Button = styled.button`
         transition : 0.3s;
     }
 `;
+
+export const NoticeButton = styled(Button)`
+    position : absolute;
+    bottom: 20%;
+    right : 15%;
+`;
+
+export const Loading = styled.div`
+    position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: rgba(0, 0, 0, 0.4);
+	z-index: 20;
+`;
